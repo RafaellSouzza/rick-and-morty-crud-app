559b80deffed04acd65bbc396d53960362966429
Rafael Soares de Souza <rafael.sd.sousa@bradesco.com.br>
2025-06-05 00:19:13 -0300

    inicializar projeto Angular 17 com Jest e material

---
+++ rick-and-morty-crud-app/jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  globals: {},
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/app/**/*.{ts,js}',
+    '!src/main.ts',
+    '!src/environments/**',
+    '!src/app/**/*.module.ts',
+    '!src/app/**/*.routes.ts',
+    '!src/app/**/*.config.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};

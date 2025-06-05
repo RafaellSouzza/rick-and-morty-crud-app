57b3adad9f67697e14706185e40ff90eb2229413
Rafael Souza <31011616+RafaellSouzza@users.noreply.github.com>
2025-06-05 05:11:04 -0300

    fix import path for ListaPersonagensComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ListaPersonagensComponent } from './funcionalidades/personagens/componentes/lista-personagens/lista-personagens.component';
+import { ListaPersonagensComponent } from './personagens/lista-personagens.component';
 
 export const routes: Routes = [
   { path: '', component: ListaPersonagensComponent },
